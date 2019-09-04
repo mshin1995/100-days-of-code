3689e3351766e58fc08a8d991322244507d2ef19
github username <github email address>
2019-09-03 18:52:52 -0700

    Problem 5

--- log.js
+++ log.js
@@ -38,3 +38,13 @@ var reverse = function(x) {
     let reversed = x.toString().split('').reverse().join('')
     return Math.sign(x) * parseInt(reversed)
 };
+
+//5.Plus One
+//Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+//The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+//You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+var plusOne = function(digits) {
+    let added = BigInt(digits.join('')) + BigInt(1)
+    return added.toString().split('')
+};
