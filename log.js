ecfe37225c84f98ccb3dc9e91ab07a23fc84afd8
github username <github email address>
2019-09-10 14:44:01 -0700

    Problem 7

--- log.js
+++ log.js
@@ -63,3 +63,15 @@ var singleNumber = function(nums) {
     }
     return array
 };
+
+//7.First Unique Character in a String
+//Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+var firstUniqChar = function(s) {
+    for(let i = 0; i < s.length; i++) {
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i
+        }
+    }
+    return -1
+};
